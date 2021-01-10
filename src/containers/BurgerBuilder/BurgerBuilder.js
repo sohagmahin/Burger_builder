835a92ebd403820ad1fe6a0c3630f06103b8f180
sohagmahin <sohagmahin@gmail.com>
2021-01-11 00:40:47 +0600

    L:8 v: 22 showing or hinding the modal with animation

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,6 +23,7 @@ class BurgerBuilder extends React.Component {
         },
         totalPrice: 4,
         purchasable: false,
+        purchasing: false,
     }
 
     updatePurchaseState(ingredients) {
@@ -77,6 +78,10 @@ class BurgerBuilder extends React.Component {
         this.updatePurchaseState(updatedIngredients);
     }
 
+    purchasingHandler = ()=>{
+        this.setState({purchasing: true});
+    }
+
     render() {
         let disabledInfo = {
             ...this.state.ingredients
@@ -88,7 +93,7 @@ class BurgerBuilder extends React.Component {
 
         return (
             <Aux>
-                <Modal>
+                <Modal show= {this.state.purchasing}>
                     <OrderSummary ingredients={this.state.ingredients} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
@@ -98,6 +103,7 @@ class BurgerBuilder extends React.Component {
                     disabledInfo={disabledInfo}
                     price={this.state.totalPrice}
                     purchasable={this.state.purchasable}
+                    ordered = {this.purchasingHandler}
                 />
             </Aux>
         )

1cfcc7b02beda0d4f35a320b64e7b54ca43b1beb
sohagmahin <sohagmahin@gmail.com>
2021-01-10 18:43:30 +0600

    l:8 v: 15 adding build control componenet

---
+++ src/components/BuildControls/BuildControls.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './buildControls.module.css';
+
+const buildControls = (props) => (
+        <di className= {classes.BuildControls}>
+                
+        </di>
+);
+
+export default buildControls;

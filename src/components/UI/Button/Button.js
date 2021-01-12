9da1212dc927466d81e4f9f3316686689fce983f
sohagmahin <sohagmahin@gmail.com>
2021-01-12 23:43:46 +0600

    L:8 v: 24 adding a custom button component[correct]

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './Button.module.css';
 
 const button = (props) => {
-    <button className={[classes.Button, classes[btnType]].join(' ')}>
+    <button className={[classes.Button, classes[props.btnType]].join(' ')}>
         {props.children}
     </button>
 }

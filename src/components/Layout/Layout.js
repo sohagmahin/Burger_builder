e2fe334a3eb5c60c59c7a9c3a9b33f08a23986f6
sohagmahin <sohagmahin@gmail.com>
2021-01-17 17:34:19 +0600

    L:8 v:33 reusing the backdrops

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,12 +4,24 @@ import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}> {props.children} </main>
-    </Aux>
-)
+class Layout extends React.Component {
+    state = {
+        showSideDrawer: true,
+    }
 
-export default layout;
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false });
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}> {this.props.children} </main>
+            </Aux>
+        );
+    }
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,27 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
+import Backdrops from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/aux/aux';
 
 const sideDrawer = (props) => {
+
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <Aux>
+            <Backdrops show={props.open} clicked={props.closed} />
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Aux>
     );
 }
 

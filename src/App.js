6e0e29ea2150ec8e85beb1efd49e521f7b3125dd
sohagmahin <sohagmahin@gmail.com>
2021-02-05 19:37:09 +0600

    Removing test code

--- src/App.js
+++ src/App.js
@@ -3,20 +3,11 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import React from 'react';
 
 class App extends React.Component {
-  state = {
-    show: true,
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ show: false });
-    }, 5000);
-  }
   render() {
     return (
       <div>
         <Layout>
-          {this.state.show ? <BurgerBuilder /> : null}
+          <BurgerBuilder />
         </Layout>
       </div>
     );

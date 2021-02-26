27e0bd3c0447561c850bcba1a5805bfec940895f
sohagmahin <sohagmahin@gmail.com>
2021-02-26 12:20:31 +0600

    L12 v3 setting up routing & routes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import React from 'react';
 import Checkout from './containers/Checkout/Checkout';
-
+import { Route, Switch } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
-          <Checkout />
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );

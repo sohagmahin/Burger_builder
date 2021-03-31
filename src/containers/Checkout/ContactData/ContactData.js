f2e759791ad90de394ebb6689d793ad5ff4d39cd
sohagmahin <sohagmahin@gmail.com>
2021-03-31 16:58:46 +0600

    L13 v15 Working on an error

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -82,6 +82,7 @@ class ContactData extends Component {
                         { value: 'cheapest', displayValue: 'Cheapest' }
                     ]
                 },
+                validation: {},
                 value: '',
                 valid: true
             },
@@ -118,6 +119,11 @@ class ContactData extends Component {
 
     checkValidity = (value, rules) => {
         let isValid = true;
+
+        if (!rules) {
+            return true;
+        }
+
         if (rules.required) {
             isValid = value.trim() !== "" && isValid;
         }

b4e26fd3d8912612845f6179dc67eca23fea45b8
sohagmahin <sohagmahin@gmail.com>
2021-05-09 17:49:07 +0600

    L17 v20 Refactoring reducer continued :fire:

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -14,38 +14,44 @@ const INGREDIENTS_PRICE = {
     meat: 1.2,
 }
 
+const addIngredients = (state, action) => {
+    const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 };
+    const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+    const updatedState = {
+        ingredients: updatedIngredients,
+        totalPrice: state.totalPrice + INGREDIENTS_PRICE[action.ingredientName]
+    };
+    return updateObject(state, updatedState);
+}
+
+const removeIngredients = (state, action) => {
+    const updatedIng = { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 };
+    const updatedIngs = updateObject(state.ingredients, updatedIng);
+    const updatedSt = {
+        ingredients: updatedIngs,
+        totalPrice: state.totalPrice - INGREDIENTS_PRICE[action.ingredientName]
+    };
+    return updateObject(state, updatedSt);
+}
+
+const setIngredients = (state, action) => {
+    return updateObject(state, {
+        ingredients: action.ingredients,
+        totalPrice: 4.00,
+        error: false
+    });
+}
+
+const fetchIngredientsFail = (state, action) => {
+    return updateObject(state, { error: true });
+}
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.ADD_INGREDIENTS:
-            const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 };
-            const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
-            const updatedState = {
-                ingredients: updatedIngredients,
-                totalPrice: state.totalPrice + INGREDIENTS_PRICE[action.ingredientName]
-            };
-            return updateObject(state, updatedState);
-
-        case actionTypes.REMOVE_INGREDIENTS:
-            const updatedIng = { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 };
-            const updatedIngs = updateObject(state.ingredients, updatedIng);
-            const updatedSt = {
-                ingredients: updatedIngs,
-                totalPrice: state.totalPrice - INGREDIENTS_PRICE[action.ingredientName]
-            };
-            return updateObject(state, updatedSt);
-
-        case actionTypes.SET_INGREDIENTS:
-            return updateObject(state, {
-                ingredients: action.ingredients,
-                totalPrice: 4.00,
-                error: false
-            });
-
-        case actionTypes.FETCH_INGREDIENTS_FAILED:
-            return updateObject(state, { error: true });
-
-        default:
-            return state;
+        case actionTypes.ADD_INGREDIENTS: return addIngredients(state, action);
+        case actionTypes.REMOVE_INGREDIENTS: return removeIngredients(state, action);
+        case actionTypes.SET_INGREDIENTS: return setIngredients(state, action);
+        case actionTypes.FETCH_INGREDIENTS_FAILED: return fetchIngredientsFail(state, action);
+        default: return state;
     }
 }
 
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -7,35 +7,52 @@ const initialState = {
     loading: false
 };
 
+const purchaseInit = (state, action) => {
+    return updateObject(state, { purchased: false });
+}
+
+const purchaseBurgerStart = (state, action) => {
+    return updateObject(state, { loading: true });
+}
+
+const purchaseBurgerSuccess = (state, action) => {
+    const newOrder = updateObject({ ...action.orderData }, { id: action.orderId });
+    return updateObject(state, {
+        loading: false,
+        purchased: true,
+        orders: state.orders.concat(newOrder)
+    });
+}
+
+const purchaseBurgerFail = (state, action) => {
+    return updateObject(state, { loading: false });
+}
+
+const fetchOrderInit = (state, action) => {
+    return updateObject(state, { loading: true, });
+}
+
+const fetchOrderSuccess = (state, action) => {
+    return updateObject(state, {
+        orders: action.orders,
+        loading: false
+    });
+}
+
+const fetchOrderFail = (state, action) => {
+    return updateObject(state, { loading: false });
+}
 const reducer = (state = initialState, action) => {
 
     switch (action.type) {
-        case actionTypes.PURCHASE_INIT:
-            return updateObject(state, { purchased: false });
-        case actionTypes.PURCHASE_BURGER_START:
-            return updateObject(state, { loading: true });
-        case actionTypes.PURCHASE_BURGER_SUCCESS:
-            const newOrder = updateObject({ ...action.orderData }, { id: action.orderId });
-            return updateObject(state, {
-                loading: false,
-                purchased: true,
-                orders: state.orders.concat(newOrder)
-            });
-        case actionTypes.PURCHASE_BURGER_FAIL:
-            return updateObject(state, { loading: false });
-        case actionTypes.FETCH_ORDERS_INIT:
-            return updateObject(state, { loading: true, });
-
-        case actionTypes.FETCH_ORDERS_SUCCESS:
-            return updateObject(state, {
-                orders: action.orders,
-                loading: false
-            });
-
-        case actionTypes.FETCH_ORDERS_FAIL:
-            return updateObject(state, { loading: false });
-        default:
-            return state;
+        case actionTypes.PURCHASE_INIT: return purchaseInit(state, action);
+        case actionTypes.PURCHASE_BURGER_START: return purchaseBurgerStart(state, action);
+        case actionTypes.PURCHASE_BURGER_SUCCESS: return purchaseBurgerSuccess(state, action);
+        case actionTypes.PURCHASE_BURGER_FAIL: return purchaseBurgerFail(state, action);
+        case actionTypes.FETCH_ORDERS_INIT: return fetchOrderInit(state, action);
+        case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrderSuccess(state, action);
+        case actionTypes.FETCH_ORDERS_FAIL: return fetchOrderFail(state, action);
+        default: return state;
     }
 }
 

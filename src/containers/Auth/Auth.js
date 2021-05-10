e99a0ffd7dbefea4c6bffbbbc143be4e4551db03
sohagmahin <sohagmahin@gmail.com>
2021-05-11 02:18:56 +0600

    L18 v8 Storing the token

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -39,7 +39,7 @@ class Auth extends Component {
                 touched: false
             },
         },
-        isSignUp: true,
+        isSignUp: false,
     }
 
     checkValidity = (value, rules) => {
